7775a611855bb6f53b4bd6cdac8cac119caecacf
Thomas C Bourgeois <bourgeot0@gmail.com>
2014-09-27 17:26:33 -0700

    Working node creation query
    uses transactional endpoint

--- neo-access.js
+++ neo-access.js
@@ -1,17 +1,13 @@
-//returns the service object which has all the urls in it.
-function neoSetup() {
+
 
 var request,
-	neoServiceRoot = 'http://localhost:7474/db/data/',
-	service = {},
-	xhr = new XMLHttpRequest(),
 	i,j,
 	people = [ 
-		{name: 'Shawn', birthDay: '22', birthMonth: 'March', birthYear: '1965', children: ['Yvonne'], parents: ['Margo', 'Paul']}, 
-		{name:'Thomas', birthDay: '19', birthMonth: 'December', birthYear: '1964', children: ['Yvonne'], parents: ['Michael', 'Verna']},
-		{name: 'Yvonne', birthDay: '01', birthMonth: 'March', birthYear: '1995',parents: ['Thomas', 'Shawn']},
-		{name: 'Michael', birthDay: '16', birthMonth: 'November', birthYear: '1942',  children: ['Thomas'], parents: ['Walter', 'Marion']},
-		{name: 'Verna', birthDay: '01', birthMonth: 'November', birthYear: '1937', children: ['Mark', 'Celeste', 'Christopher', 'Thomas'], parents: ['Rose']}
+		{name: 'Shawn', key: "shawnbourgeois", birthDay: '22', birthMonth: 'March', birthYear: '1965', children: ['Yvonne'], parents: ['Margo', 'Paul']}, 
+		{name:'Thomas', key: "thomasbourgeois", birthDay: '19', birthMonth: 'December', birthYear: '1964', children: ['Yvonne'], parents: ['Michael', 'Verna']},
+		{name: 'Yvonne', key: "yvonnebourgeois", birthDay: '01', birthMonth: 'March', birthYear: '1995',parents: ['Thomas', 'Shawn']},
+		{name: 'Michael', key: "michaelbourgeois", birthDay: '16', birthMonth: 'November', birthYear: '1942',  children: ['Thomas'], parents: ['Walter', 'Marion']},
+		{name: 'Verna', key: "vernabourgeois", birthDay: '01', birthMonth: 'November', birthYear: '1937', children: ['Mark', 'Celeste', 'Christopher', 'Thomas'], parents: ['Rose']}
 	];
 	//xhr.open(method, url, async)...send(post request)
 	/* 
@@ -19,7 +15,10 @@ var request,
 xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 xmlhttp.send("fname=Henry&lname=Ford")
 */	
-function getService() {
+function setup() {
+//returns the service object which has all the urls in it.
+	var 	neoServiceRoot = 'http://localhost:7474/db/data/',
+		xhr = new XMLHttpRequest();
 	xhr.open('GET', neoServiceRoot, true);	
 	xhr.setRequestHeader("Access-Control-Allow-Origin", '*');
 	xhr.setRequestHeader("Accept", "application/json; charset=UTF-8");
@@ -29,14 +28,52 @@ function getService() {
 		if (xhr.readyState==4 && xhr.status==200) {
 			//document.getElementById("response").innerHTML=xhr.responseText;
 			service = JSON.parse(xhr.responseText);
-			console.log(service);
-			return service;
+			//console.log(service);
+			//
+			load(service);
 		}
 	}
 }
 //run a query and return a result.
 
-
+function load(service) {
+	//assume data is a list of objects. add them if they haven't already been added based on 'key' value
+	//NOT YET--> let each property be a node with name and key attrs same as value, and add a relationship 'has' for it.
+	//CREATE (n:Person { name : 'Andres', title : 'Developer' })
+	/* {
+  "statements" : [ {
+    "statement" : "CREATE (n {props}) RETURN n",
+    "parameters" : {
+      "props" : {
+        "name" : "My Node"
+      }
+    }
+  } ]
+} */
+	var data = people;
+	var result,
+		query = {statements:[]};
+	var cQ = {statement: 'CREATE (n:Person {nodes}) RETURN n', parameters: {}};
+	cQ.parameters = {nodes: data};
+	query.statements.push(cQ);
+	//query.params = {nodes: []};
+	
+	//console.log(query);
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", service.transaction + '/commit', true);
+		xhr.setRequestHeader("Access-Control-Allow-Origin", '*');
+		xhr.setRequestHeader("Accept", "application/json; charset=UTF-8");
+		//xhr.setRequestHeader("X-Stream", "true");
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.send(JSON.stringify(query));
+		xhr.onreadystatechange=function() {
+			if (xhr.readyState==4 && xhr.status==200) {
+				//document.getElementById("response").innerHTML=xhr.responseText;
+				result = JSON.parse(xhr.responseText);
+				console.log(result);
+			}
+		}
+}
 function neoQuery(query, result) {
-
+	
 }

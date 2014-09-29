5a0abf5f8b3383561e1650a0677c60c73e406aa1
Thomas C Bourgeois <bourgeot0@gmail.com>
2014-09-28 23:12:44 -0700

    utlilities as closures maybe
    flailing for the moment. prossibly a waste of time.

---
+++ neo-test.js
@@ -0,0 +1,26 @@
+var neo4J = (function() {
+	var aService;
+	function service() {
+		if(aService === undefined) {
+		//returns the service object which has all the urls in it.
+			var 	neoServiceRoot = 'http://localhost:7474/db/data/',
+				xhr = new XMLHttpRequest();
+			xhr.open('GET', neoServiceRoot, true);	
+			xhr.setRequestHeader("Access-Control-Allow-Origin", '*');
+			xhr.setRequestHeader("Accept", "application/json; charset=UTF-8");
+			xhr.setRequestHeader("X-Stream", "true");
+			xhr.send();
+			xhr.onreadystatechange=function() {
+				if (xhr.readyState==4 && xhr.status==200) {
+					//document.getElementById("response").innerHTML=xhr.responseText;
+					aService = JSON.parse(xhr.responseText);
+					//console.log(service);
+					//load(service);
+				}
+			}
+		}
+		else {
+			return aService;
+		}
+	}
+})();
